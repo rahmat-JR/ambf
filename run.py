97e92db0dac999d5a29073870fa1304f39b49352
Angga Kurniawan <67410461+anggaxd@users.noreply.github.com>
2021-09-07 09:41:16 +0700

    Create run.py

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
-import sys, os
-
-if sys.version[0:3] != "3.9":
-	sys.exit("[+] anda harus menggunakan versi python 3.9, versi python anda sekarang : "+sys.version[0:3])
-
 if __name__ == "__main__":
   try:
   	__import__("ambf").main_apikey()
