a4899506b1bb3f7eb254d0fa11242a8c89eeb7eb
Angga Kurniawan <67410461+anggaxd@users.noreply.github.com>
2021-09-15 22:41:14 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
+import os, sys
 if __name__ == "__main__":
-  try:
-  	__import__("ambf").main_apikey()
-  except Exception as e:
-  	exit(str(e))
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "remove":
+			os.system("rm -f .__apikey__.txt")
+			exit(" [!] berhasil menghapus api key")
+		else:
+			print(" [?] cara menggunakan : ")
+			exit(" [!] ketik : python run.py remove")
+	try:
+		__import__("ambf").main()
+	except Exception as e:
+		exit(str(e))
